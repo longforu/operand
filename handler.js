0b691743102dc7d06653b9208a149c5c89e6e60a
Morgan Gallant <morgan@morgangallant.com>
2021-08-15 16:59:43 -0400

    back to echo

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
 // This is a test handler which echos back the messsage that the user sent.
 const handler = async (req) => {
-    operand.respond("hello cat <3");
+    operand.respond(req.message);
 }
